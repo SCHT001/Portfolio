9b33b833af9beeec3c69e0eff9715fecb8790e50
Sachet Subedi <sachetsubedi001@gmail.com>
2023-07-04 19:02:19 +0545

    commentsremoved

--- script.js
+++ script.js
@@ -2,18 +2,7 @@
 var themeBtn=document.getElementById('themeToggle');
 var theme="dark";
 var body=document.getElementById('htmlBody');
-// themeBtn.addEventListener('click',()=>{
-//     if(theme=="dark"){
-//         theme="light";
-//         themeBtn.innerHTML=`<i class="fas fa-moon"></i>`;
-//         body.classList.replace('dark-theme','light-theme'); 
-//     }
-//     else{
-//         theme="dark";
-//         themeBtn.innerHTML=`<i class="fas fa-sun"></i>`; 
-//         body.classList.replace('light-theme','dark-theme'); 
-//     }
-// })
+
 var colors=document.querySelectorAll('.colorSets');
 console.log(colors);
 colors.forEach((color) => {
