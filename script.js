09ca33556acd2e69719d58ebdddf0759fd6cd48a
SCHT001 <sachetsubedi001@gmail.com>
2023-09-14 10:01:38 +0545

    animations adjust

--- script.js
+++ script.js
@@ -1,31 +1,33 @@
+var theme = "dark";
+var body = document.getElementById('htmlBody');
 
-var themeBtn=document.getElementById('themeToggle');
-var theme="dark";
-var body=document.getElementById('htmlBody');
-
-var colors=document.querySelectorAll('.colorSets');
+var themeBtn=document.querySelectorAll('.themeBtn');
+var colors = document.querySelectorAll('.colorSets');
 console.log(colors);
 colors.forEach((color) => {
-    color.addEventListener('click',(e)=>{
-        body.classList.remove('text-lime','text-blue','text-yellow','text-red','text-purple');
-        body.classList.add('text-'+(e.target.dataset.color));
+    color.addEventListener('click', (e) => {
+        body.classList.remove('text-lime', 'text-blue', 'text-yellow', 'text-red', 'text-purple');
+        body.classList.add('text-' + (e.target.dataset.color)); 
+        colors.forEach((color)=>{
+            color.classList.remove('active');
+        })
+        e.target.classList.add('active');
+       
     })
 });
-var themeBtn=document.querySelectorAll('.themeBtn');
+
 themeBtn.forEach((btn)=>{
-    btn.addEventListener('click',()=>{ 
-        if(theme=="dark"){
-                    theme="light";
-                    themeBtn.innerHTML=`<i class="fas fa-moon"></i>`;
-                    body.classList.replace('dark-theme','light-theme'); 
-                }
-                else{
-                    theme="dark";
-                    themeBtn.innerHTML=`<i class="fas fa-sun"></i>`; 
-                    body.classList.replace('light-theme','dark-theme'); 
-                }
-    })
+
+    btn.addEventListener('click', () => {
+        if (theme == "dark") {
+            theme = "light";
+            btn.innerHTML = `<i class="fas fa-moon"></i>`;
+            body.classList.replace('dark-theme', 'light-theme');
+        }
+        else {
+            theme = "dark";
+            btn.innerHTML = `<i class="fas fa-sun"></i>`;
+            body.classList.replace('light-theme', 'dark-theme');
+        }
+    });
 })
-
-
-
