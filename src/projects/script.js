ed7eb55c45410d8b2455150f97f9afc79c125efa
SCHT001 <sachetsubedi001@gmail.com>
2023-10-18 09:20:23 +0545

    random saves

--- src/projects/script.js
+++ src/projects/script.js
@@ -18,5 +18,18 @@ function updateList(){
     projects.forEach((project)=>{
         let projectItem=document.createElement('div');
         projectItem.classList.add('projectItem');
+        projectItem.innerHTML=`<div class="forFlex">
+        <div class="projectBody" id="projectList">
+            <span class="projectHeader"><h2>${project.head}</h2></span>
+            <span class="projectDes">${project.des}</span>
+        </div>
+        <div class="projectButtons">
+            <span class="tryitBtn "><a class="rounded" href="">${project.tryitLink}</a></span>
+            <span class="githubBtn "><a class="rounded" href="">${project.githubLink} </a></span>
+        </div>
+    </div>
+    <div class="expander"><i class="fa-solid fa-angle-down"></i></div>`
+    projectList.appendChild(projectItem);
     })
 }
+updateList();
